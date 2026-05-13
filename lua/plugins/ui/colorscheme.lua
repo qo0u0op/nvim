db9ad87557fd35c773c011a27fd00628bc28df42
qo0u0op <vikrolinne07@outlook.com>
2026-05-13 20:45:13 +0800

    chore: move troblue.lua, remove unused colorscheme, format lua script

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,13 +1,10 @@
 return {
   -- add colorscheme:
-  { "sainnhe/gruvbox-material" },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    --"catppuccin-frappe"
   },
-  -- use colorscheme:
   {
     "LazyVim/LazyVim",
     opts = {
--- lua/plugins/ui/yazi.lua
+++ lua/plugins/ui/yazi.lua
@@ -1,44 +1,42 @@
 return {
-  {
-    "mikavilpas/yazi.nvim",
-    version = "*", -- use the latest stable version
-    event = "VeryLazy",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", lazy = true },
-    },
-    keys = {
-      -- 👇 in this section, choose your own keymappings!
-      {
-        "<leader>y",
-        mode = { "n", "v" },
-        "<cmd>Yazi<cr>",
-        desc = "Explorer Yazi",
-      },
-      {
-        -- Open in the current working directory
-        "<leader>Y",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Explorer Yazi (cwd)",
-      },
-      {
-        "<c-\\>",
-        "<cmd>Yazi toggle<cr>",
-        desc = "Resume Yazi",
-      },
-    },
-    opt = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = true,
-      keymaps = {
-        show_help = "<f1>",
-      },
-    },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
-    init = function()
-      -- mark netrw as loaded so it's not loaded at all.
-      --
-      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-      vim.g.loaded_netrwPlugin = 1
-    end,
+  "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
   },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>y",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Explorer Yazi",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>Y",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Explorer Yazi (cwd)",
+    },
+    {
+      "<c-\\>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume Yazi",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = true,
+    keymaps = {
+      show_help = "~",
+    },
+  },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- mark netrw as loaded so it's not loaded at all.
+    --
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
