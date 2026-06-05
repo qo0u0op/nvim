da53e7d41ad8f59ee0c9b9fca09203b14195ae53
qo0u0op <vikrolinne07@outlook.com>
2026-06-05 21:29:45 +0800

    chore: change to mise vm

--- lua/plugins/lang/pyright.lua
+++ lua/plugins/lang/pyright.lua
@@ -8,11 +8,11 @@ return {
             python = {
               -- 動態設定 pythonPath
               pythonPath = (function()
-                -- 執行 `asdf which python` 來取得當前 asdf 環境的 python 路徑
-                local asdf_python = vim.fn.trim(vim.fn.system("asdf which python"))
+                -- 執行 `mise which python` 來取得當前 mise 環境的 python 路徑
+                local mise_python = vim.fn.trim(vim.fn.system("mise which python"))
                 -- vim.v.shell_error 會在最後一個 shell 命令失敗時設定為非零值
                 if vim.v.shell_error == 0 then
-                  return asdf_python
+                  return mise_python
                 end
                 -- 如果命令失敗，返回 nil，讓 pyright 使用預設的 python
                 return nil
