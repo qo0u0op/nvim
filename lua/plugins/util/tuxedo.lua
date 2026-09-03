ceb687a0cc05ef9a56b7a0e47134dc08e8cd39e5
qo0u0op <vikrolinne07@outlook.com>
2026-09-03 16:40:56 +0800

    feat: add tuxedo todo wrapper

---
+++ lua/plugins/util/tuxedo.lua
@@ -0,0 +1,11 @@
+return {
+  "IogaMaster/tuxedo.nvim",
+  opts = {
+    create_todo_file = true,
+    width_ratio = 0.95,
+    height_ratio = 0.80,
+  },
+  keys = {
+    { "<leader>kd", "<cmd>Tuxedo<cr>", desc = "Tuxedo" },
+  },
+}
